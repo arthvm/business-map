8e156e84195de20de50e00ce864bc323572b4170
Arthur Mariano <arthvm@proton.me>
2025-08-13 15:32:58 -0300

    Initial Commit

---
+++ BusinessMap/BusinessMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessMapApp.swift
+//  BusinessMap
+//
+//  Created by Arthur Mariano on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BusinessMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
